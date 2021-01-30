a161c62c1ee985f8039040a646148eb892ccaf8a
zlamaann <andapanda.az@gmail.com>
2021-01-30 19:46:09 +0100

    Plane import

---
+++ Assets/Camera_Follow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public Transform PLAYER;
+
+    [SerializeField] private float Smoothing = 0.2f;
+
+    [SerializeField] private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // PŘIDAT - Letadlo přelétá do "view" při startu ve vzduchu (bool)
+
+        // Přidat - Camera funguje až poté co je na správné vzdálenosti od Follow_for_Camera
+
+        // PŘIDAT - Automaticky při startu určit objekt pro pozici PLAYER
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // PŘIDAT - Oddalování a přibližování pomocí kolečka myši
+        // PŘIDAT - Omezit Přiblížení a oddálení
+
+        // PŘIDAT - Rozhlížení se pomocí držení druhého tlačítka myši
+
+        // PŘIDAT - Rotace kamery dolů při přiblížení
+
+        // Přidat - Zabránit kameře prolétávat objekty.
+    }
+
+    // Called after update
+    void FixedUpdate()
+    {
+        Vector3 posBeforeSmoothing = PLAYER.position + offset;
+        Vector3 posAfterSmoothing = Vector3.Lerp(transform.position, posBeforeSmoothing, Smoothing);
+        transform.position = posAfterSmoothing;
+
+        transform.LookAt(PLAYER);
+    }
+}

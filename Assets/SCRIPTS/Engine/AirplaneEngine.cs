fda5292c5938bf11c5c20c6e4e6f2875a2ef7dcd
zlamaann <andapanda.az@gmail.com>
2021-02-14 00:10:19 +0100

    Engine animation curve

--- Assets/SCRIPTS/Engine/AirplaneEngine.cs
+++ Assets/SCRIPTS/Engine/AirplaneEngine.cs
@@ -7,12 +7,16 @@ public class AirplaneEngine : MonoBehaviour
     #region Variables
     public float maxForce = 200f;
     public float maxRPM = 1200f;
+
+    public AnimationCurve powerCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
     #endregion
 
     #region Methods
     public Vector3 CalculateForce(float throttle)
     {
         float finalThrottle = Mathf.Clamp01(throttle);
+        finalThrottle = powerCurve.Evaluate(finalThrottle);
+
         float finalPower = finalThrottle * maxForce;
 
         Vector3 finalForce = transform.forward * finalPower;
